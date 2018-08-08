3776e2fe68ee0a673f0393022c76178d8b4bfe09
sanlucca <sgoldscheider@lucca.fr>
2018-08-08 16:20:02 +0200

    [rem] Wrong Import

--- src/app/nav-side/nav-side-group/nav-side-group.component.ts
+++ src/app/nav-side/nav-side-group/nav-side-group.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '../../../../node_modules/@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'ds-nav-side-group',
