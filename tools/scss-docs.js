8cbd4f6833255dd3cddea92f22011b8b7ca6b3bd
sanlucca <sgoldscheider@lucca.fr>
2018-08-27 13:46:25 +0200

    [fix] Handle Colors table and multi number values (! need a fix on LF/scss)

--- tools/scss-docs.js
+++ tools/scss-docs.js
@@ -8,19 +8,26 @@ let glob = require('glob');
 const distDir = './api';
 const fileName = 'scss-theme.api.ts';
 const fullFilePath = `${distDir}/${fileName}`;
-
 const sourceFiles = glob.sync("node_modules/@lucca-front/scss/src/theming/**/*.scss", {
 	ignore: ['**/_components.scss', '**/_get-set.scss', '**/_utils.scss']
 });
 
 const content = sourceFiles.reduce((soFar, file) => {
 	let fileContent = fs.readFileSync(file).toString();
+	fileContent = fileContent.replace(/: [^\(](.*?)$/gm, singleQuoteValues);
+
+	// Remove all comments
+	fileContent = fileContent.replace(/\/\*[\s\S]*?\*\/|([^:"]|^)\/\/.*$/gm, "");
+
+	// Format content
+	fileContent = fileContent.replace(/,(\s*?)(?=\))/gm, "");
+	fileContent = fileContent.replace(/\s\s+/gm, " ");
 	fileContent = fileContent.replace(/(\r\n|\n|\r|\t)/gm, "");
 	soFar += fileContent;
 	return soFar;
 }, '');
 
-const scssContent = sast.parse(content);
+const scssContent = sast.parse(content, {syntax : 'scss'});
 
 let finalContent = 'const SCSS_DOCS = {';
 
@@ -52,7 +59,7 @@ function forgeNode(key, val) {
 	if (typeof val === "object") {
 		return forgeMapNode(key, val);
 	} else {
-		val = val.toString();
+		val = removeSingleQuote(val.toString());
 		return forgeValueNode(key, val);
 	}
 }
@@ -67,4 +74,24 @@ function forgeMapNode(key, val) {
 		name: key,
 		children: Object.keys(val).map(key => forgeNode(key, val[key])),
 	};
+}
+
+function removeSingleQuote(s) {
+	if (s.startsWith('\'')) {
+		s = s.slice(1);
+	}
+	if (s.endsWith('\'')) {
+		s = s.slice(0, -1);
+	}
+	return s;
+}
+
+function singleQuoteValues(car) {
+	let endchar = '';
+	if(car.endsWith(',') | car.endsWith(';')) {
+		endchar = car[car.length-1];
+		car = car.slice(0, -1);
+	}
+	car = car.slice(2);
+	return ': \''+ car + '\'' + endchar;
 }
