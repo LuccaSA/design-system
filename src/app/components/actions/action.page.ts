70fe584b7d74fd4a7df9c36607c7347ca6fef618
lucienbertin <me@lucienbertin.com>
2018-09-06 17:10:56 +0200

    add pages for components > actions > button

---
+++ src/app/components/actions/action.page.ts
@@ -0,0 +1,6 @@
+import { GroupPage } from '../../commons';
+import { buttonsPage } from './buttons/buttons.page';
+
+export const actionsPage = new GroupPage('actions', 'Actions', [
+	buttonsPage,
+]);
---
+++ src/app/components/components.page.ts
@@ -0,0 +1,6 @@
+import { GroupPage } from '../commons';
+import { actionsPage } from './actions/action.page';
+
+export const componentsPage = new GroupPage('components', 'Components', [
+	actionsPage,
+]);
