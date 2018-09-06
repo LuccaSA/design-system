8616e743f7a99d415133742bb1acd9b8e4623469
sanlucca <sgoldscheider@lucca.fr>
2018-09-06 11:56:05 +0200

    [fix] routerLinks were broken on various component

--- src/app/components/layout/containers/containers.module.ts
+++ src/app/components/layout/containers/containers.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { DsCommonsModule } from '../../../commons';
 import { BasicExampleComponent } from './basic/basic.example';
 import { ContainersComponent } from './containers.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-	imports: [DsCommonsModule],
+	imports: [
+		DsCommonsModule,
+		RouterModule,
+	],
 	declarations: [
 		ContainersComponent,
 		BasicExampleComponent
