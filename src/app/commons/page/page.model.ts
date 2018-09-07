d28f0856f404e1544468318fd2cea8abe827eb09
lucienbertin <me@lucienbertin.com>
2018-09-07 10:01:21 +0200

    clean code

--- src/app/commons/page/page.model.ts
+++ src/app/commons/page/page.model.ts
@@ -1,5 +1,5 @@
 import { Route } from '@angular/router';
-import { NavSideComponent } from "../../nav-side/nav-side.component";
+import { NavSideComponent } from '../../nav-side/nav-side.component';
 
 export interface IPage {
 	title: string;
@@ -60,12 +60,6 @@ export abstract class APage implements IPage {
 	}
 	abstract toRoute();
 	abstract toIndex(): IPage[];
-	//  {
-	// 	const clone = Object.create(this);
-	// 	const children = clone.children as IPage[];
-	// 	clone._children = [];
-	// 	return [clone, ...children.map(c => c.toIndex()).reduce((acc, cur) => [...acc, ...cur], [])];
-	// }
 }
 
 export class FeaturePage extends APage implements IPage {
@@ -115,108 +109,4 @@ export class RootGroupPage extends GroupPage implements IPage {
 			],
 		} as Route;
 	}
-
 }
-
-// export class Pages {
-// 	constructor(
-// 		public path: string,
-// 		public pages: IPage[],
-// 		public defaultPage: Page = pages[0]
-// 	) {}
-
-// 	public toNav() {
-// 		return this.pages.filter(page => {
-// 			return !page.parent || !this.pages.includes(page.parent);
-// 		}).map( page => page.toNav(this.pages));
-// 	}
-
-// 	public toIndex() {
-// 		return this.pages.map( page => {
-// 			if (!page.parent) {
-// 				page.groupPath = this.path;
-// 			}
-// 			return page;
-// 		}, [])
-// 		.filter(page => page.component); // removes all "Group"
-// 	}
-
-// 	public toRoutes(): Routes {
-// 		const routes = this.pages.filter( page => page.component ).map(page => page.toRoute(this.pages), []);
-// 		return [
-// 			{ path: this.path, children: [
-// 				{ path: '', component: NavSideComponent, data: {pages: this}, outlet: 'navSide'},
-// 				{ path: '', redirectTo: `/${this.path}/${this.defaultPage.fullPath}`, pathMatch: 'full' },
-// 				...routes
-// 			]}
-// 		];
-// 	}
-// }
-// export class Page implements IPage {
-// 	public groupPath: string;
-// 	public parent: IPage;
-// 	get fullPath(): string {
-// 		if (!this.parent) {
-// 			return this.groupPath ? `${this.groupPath}/${this.path}` : this.path;
-// 		}
-// 		return this.breadcrumb.map(b => {
-// 			return b.groupPath ? `${b.groupPath}/${b.path}` : b.path;
-// 		}).join('/');
-// 	}
-
-// 	get breadcrumb(): this[] {
-// 		const breadcrumbs: this[] = [];
-// 		let currentPage: this = this;
-// 		while (currentPage != null) {
-// 			breadcrumbs.unshift(currentPage);
-// 			currentPage = currentPage.parent;
-// 		}
-// 		return breadcrumbs;
-// 	}
-
-// 	constructor(
-// 		public path: string,
-// 		public component: any,
-// 		public title: string,
-// 		// public parent: Page = null,
-// 		public keywords: string[] = [],
-// 	) { }
-
-// 	public toRoute(pages: Page[]): Route {
-// 		return { path: this.fullPath, component: this.component };
-// 	}
-
-// 	public toNav(pages: Page[]) {
-// 		const children = pages.filter(child => {
-// 			return child.parent === this;
-// 		});
-// 		const navChildren = children.map(page => page.toNav(pages), []);
-// 		if (navChildren.length < 1) {
-// 			return { page: this };
-// 		}
-// 		return { groupPage: this, children: navChildren };
-// 	}
-
-// 	public match(clue: string): boolean {
-// 		const normalizedClue = this._normalizeString(clue);
-// 		if (this._normalizeString(this.title).indexOf(normalizedClue) !== -1) {
-// 			return true;
-// 		}
-// 		if (this.parent && this._normalizeString(this.parent.title).indexOf(normalizedClue) !== -1) {
-// 			return true;
-// 		}
-// 		for (const keyword of this.keywords) {
-// 			if (this._normalizeString(keyword).indexOf(normalizedClue) !== -1) {
-// 				return true;
-// 			}
-// 		}
-// 		return false;
-// 	}
-
-// 	private _normalizeString(str: string): string {
-// 		return str
-// 			.normalize('NFD')
-// 			.replace(/[\u0300-\u036f]/g, '')
-// 			.toLowerCase();
-// 	}
-// }
