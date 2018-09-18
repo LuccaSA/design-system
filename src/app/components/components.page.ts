ef7df96a6d2fa57a3ab5d3628d14ab082b5e3976
lucienbertin <me@lucienbertin.com>
2018-09-18 15:01:50 +0200

    migrate components/listings

--- src/app/components/components.page.ts
+++ src/app/components/components.page.ts
@@ -3,10 +3,12 @@ import { actionsPage } from './actions/action.page';
 import { feedbackPage } from './feedback/feedback.page';
 import { formPage } from './form/form.page';
 import { layoutPage } from './layout/layout.page';
+import { listingsPage } from './listings/listings.page';
 
 export const componentsPage = new RootGroupPage('components', 'Components', [
 	actionsPage,
 	feedbackPage,
 	formPage,
 	layoutPage,
+	listingsPage,
 ]);
