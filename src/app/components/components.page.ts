59b602b79a20785fc620f3d3b0f03adcf5614e94
lucienbertin <me@lucienbertin.com>
2018-09-18 15:00:19 +0200

    migrate components/layout

--- src/app/components/components.page.ts
+++ src/app/components/components.page.ts
@@ -2,9 +2,11 @@ import { RootGroupPage } from '../commons';
 import { actionsPage } from './actions/action.page';
 import { feedbackPage } from './feedback/feedback.page';
 import { formPage } from './form/form.page';
+import { layoutPage } from './layout/layout.page';
 
 export const componentsPage = new RootGroupPage('components', 'Components', [
 	actionsPage,
 	feedbackPage,
 	formPage,
+	layoutPage,
 ]);
---
+++ src/app/components/layout/containers/containers.page.ts
@@ -0,0 +1,8 @@
+import { FeaturePage } from '@ds/commons';
+import { ContainersComponent } from './containers.component';
+
+export const containersPage = new FeaturePage(
+	'containers',
+	'Containers',
+	ContainersComponent,
+);
