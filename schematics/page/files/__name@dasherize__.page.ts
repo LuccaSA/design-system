5afb47970981a9ee0843e80586d708a98b9d1534
lucienbertin <me@lucienbertin.com>
2018-09-18 11:40:00 +0200

    schems - camelize page name

--- schematics/page/files/__name@dasherize__.page.ts
+++ schematics/page/files/__name@dasherize__.page.ts
@@ -1,7 +1,7 @@
 import { FeaturePage } from '@ds/commons';
 import { <%= classify(name) %>Component } from './<%=dasherize(name)%>.component';
 
-export const <%=dasherize(name)%>Page = new FeaturePage(
+export const <%=camelize(name)%>Page = new FeaturePage(
 	'<%=dasherize(name)%>',
 	'<%=capitalize(name)%>',
 	<%= classify(name) %>Component,
