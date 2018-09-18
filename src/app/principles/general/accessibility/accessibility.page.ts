3749fa7d65b5235411f3c0a2f5b5ef6812610099
lucienbertin <me@lucienbertin.com>
2018-09-18 11:20:13 +0200

    added all pages under /principles, generated through ng g page

---
+++ src/app/principles/general/accessibility/accessibility.page.ts
@@ -0,0 +1,8 @@
+import { FeaturePage } from '@ds/commons';
+import { AccessibilityComponent } from './accessibility.component';
+
+export const accessibilityPage = new FeaturePage(
+	'accessibility',
+	'Accessibility',
+	AccessibilityComponent,
+);
---
+++ src/app/principles/general/overlay/overlay.page.ts
@@ -0,0 +1,8 @@
+import { FeaturePage } from '@ds/commons';
+import { OverlayComponent } from './overlay.component';
+
+export const overlayPage = new FeaturePage(
+	'overlay',
+	'Overlay',
+	OverlayComponent,
+);
