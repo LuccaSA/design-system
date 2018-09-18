89362793439deca75695f565627c3f737ef3f24e
lucienbertin <me@lucienbertin.com>
2018-09-18 14:46:44 +0200

    migrate components/actions

---
+++ src/app/components/actions/links/links.page.ts
@@ -0,0 +1,8 @@
+import { FeaturePage } from '@ds/commons';
+import { LinksComponent } from './links.component';
+
+export const linksPage = new FeaturePage(
+	'links',
+	'Links',
+	LinksComponent,
+);
