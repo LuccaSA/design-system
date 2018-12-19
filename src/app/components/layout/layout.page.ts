4183cd64c11819254aae52c8407303ff06c73009
jeremie-lucca <jbiron@lucca.fr>
2018-12-19 17:57:53 +0100

    add section component

--- src/app/components/layout/layout.page.ts
+++ src/app/components/layout/layout.page.ts
@@ -2,6 +2,7 @@ import { GroupPage } from '@ds/commons';
 import { cardsPage } from './cards/cards.page';
 import { containersPage } from './containers/containers.page';
 import { emptyStatesPage } from './empty-states/empty-states.page';
+import { sectionsPage } from './sections/sections.page';
 import { gridPage } from './grid/grid.page';
 import { templatesPage } from './templates/templates.page';
 
@@ -14,5 +15,6 @@ export const layoutPage = new GroupPage(
 		gridPage,
 		cardsPage,
 		emptyStatesPage,
+		sectionsPage,
 	],
 );
