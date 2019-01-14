b33ff444d3ccc27caea87f17a927cd267b284a67
lucienbertin <me@lucienbertin.com>
2019-01-14 11:59:43 +0100

    create lib common (empty lib for now

---
+++ libraries/common/src/lib/common.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule as NgCommonModule } from '@angular/common';
+
+@NgModule({
+	imports: [
+		NgCommonModule,
+	],
+	exports: [
+		NgCommonModule,
+	],
+})
+export class CommonModule {}
