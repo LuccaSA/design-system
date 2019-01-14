982e88826b20d6f2d7b350c5e3177f1357f89267
lucienbertin <me@lucienbertin.com>
2019-01-14 17:38:07 +0100

    mov assets to prisme/assets

--- applications/prisme/src/app/home/home-footer/home-footer.component.ts
+++ applications/prisme/src/app/home/home-footer/home-footer.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
-// import { ITool } from '@prisme/common';
+import { ITool } from '@prisme/common';
 
 @Component({
 	selector: 'pri-home-footer',
 	templateUrl: './home-footer.component.html'
 })
 export class HomeFooterComponent {
-	// figma = {
-	// 	title: 'Une maquette a faire ?',
-	// 	description: 'Utilisez notre librairie figma',
-	// 	link: '#',
-	// 	href: 'assets/figma.png',
-	// } as ITool;
-	// github = {
-	// 	title: `Besoin d'une évolution ?`,
-	// 	description: 'Contribuez sur GitHub',
-	// 	link: 'https://github.com/LuccaSA/lucca-front',
-	// 	href: 'assets/github.png',
-	// } as ITool;
+	figma = {
+		title: 'Une maquette a faire ?',
+		description: 'Utilisez notre librairie figma',
+		link: '#',
+		href: 'assets/figma.png',
+	} as ITool;
+	github = {
+		title: `Besoin d'une évolution ?`,
+		description: 'Contribuez sur GitHub',
+		link: 'https://github.com/LuccaSA/lucca-front',
+		href: 'assets/github.png',
+	} as ITool;
 }
