3d7201b3ff991d199f196cd9402fd4ed8c30bbd8
jeremie-lucca <jbiron@lucca.fr>
2019-04-04 11:49:03 +0200

    examples

--- applications/prisme/src/app/components/navigation/navigation.module.ts
+++ applications/prisme/src/app/components/navigation/navigation.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@prisme/common';
 import { MenuModule } from './menu/menu.module';
 import { BreadcrumbsModule } from './breadcrumbs/breadcrumbs.module';
 import { NavSideModule } from './nav-side/nav-side.module';
+import { TableOfContentModule } from './table-of-content';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		NavSideModule,
 		MenuModule,
-		BreadcrumbsModule
+		BreadcrumbsModule,
+		TableOfContentModule
 	]
 })
 export class NavigationModule { }
